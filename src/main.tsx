2ba93198b30897cf5013c88807bc3f6f7880cb1e
flolim-mj54 <moojeong54@flolim.com>
2026-03-05 10:35:57 +0900

    main.tsx > index.css 경로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+// 🚨 경로가 수정된 부분: "./index.css" -> "./assets/index.css"
+import "./assets/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
