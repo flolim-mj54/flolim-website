d94fcfa12e5acc23941758105063f9b7d033be54
flolim-mj54 <moojeong54@flolim.com>
2026-03-05 13:47:59 +0900

    test3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         
         <div className="relative container mx-auto max-w-6xl text-center z-10">
           <h1 className="text-4xl md:text-6xl lg:text-7xl font-black mb-6 tracking-tight leading-tight">
-  <span className="text-emerald-400">스마트 IoT</span> 제어시스템 솔루션
+  <span className="text-white drop-shadow-[0_0_15px_rgba(24,169,198,0.9)]">스마트 IoT</span> 제어시스템 솔루션
 </h1>
           <p className="text-xl md:text-2xl mb-12 text-slate-300 font-light drop-shadow-md">
             에너지 소모는 줄이고 경영 효율은 더하는 <strong className="text-white font-bold">통합 관제 플랫폼</strong>
